43b696abc518e5415ca374f495e0c61e8b83faf9
vibhav <kamathpvk@gmail.com>
2024-08-31 08:21:37 +0530

    url update and angular into git ignore

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,21 +1,23 @@
-import { Injectable } from '@angular/core';
-import { HousingLocation } from './housing-location';
+import { Injectable } from "@angular/core";
+import { HousingLocation } from "./housing-location";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class HousingService {
-  url = 'http://localhost:3000/locations'
+  url = "https://backend-vibhav.onrender.com/locations";
 
-  constructor() { }
+  constructor() {}
 
   async getAllHousingLocations(): Promise<HousingLocation[]> {
     const data = await fetch(this.url);
-    return await data.json() ?? [];
+    return (await data.json()) ?? [];
   }
 
-  async getHousingLocationById(id: Number): Promise<HousingLocation | undefined> {
+  async getHousingLocationById(
+    id: Number
+  ): Promise<HousingLocation | undefined> {
     const data = await fetch(`${this.url}/${id}`);
-    return await data.json() ?? {};
+    return (await data.json()) ?? {};
   }
 }
